272f87fd506c9dc7e760935b2bacb95b092f30b8
Phineas Jensen <phin@zayda.net>
2021-07-31 19:32:32 -0600

    Improve layout and add morphological parse

--- src/main.rs
+++ src/main.rs
@@ -43,6 +43,9 @@ struct Query {
     transcription: String,
     num_syllables: Option<i32>,
     stress: Option<String>,
+    morph_orthography: Option<String>,
+    morph_transcription: Option<String>,
+    num_morphemes: Option<i32>,
     min_frequency: Option<i32>,
     max_frequency: Option<i32>,
     part_of_speech: Option<String>,
@@ -50,12 +53,12 @@ struct Query {
 
 #[derive(Serialize, Debug)]
 struct Word {
-    spelling: String,
+    orthography: String,
     transcription: String,
     num_syllables: i32,
     stress: String,
-    spelling_morph: String,
-    transcription_morph: String,
+    morph_orthography: String,
+    morph_transcription: String,
     num_morphemes: i32,
     part_of_speech: String,
     hal_frequency: i32,
@@ -68,6 +71,9 @@ fn parse_query(query: &str) -> Query {
         transcription: String::from(""),
         num_syllables: None,
         stress: None,
+        morph_orthography: None,
+        morph_transcription: None,
+        num_morphemes: None,
         min_frequency: None,
         max_frequency: None,
         part_of_speech: None,
@@ -79,23 +85,14 @@ fn parse_query(query: &str) -> Query {
                 match key {
                     "orthography" => result.orthography = value,
                     "transcription" => result.transcription = value,
-                    "syllables" => {
-                        if let Ok(num_syllables) = value.parse() {
-                            result.num_syllables = Some(num_syllables);
-                        }
-                    }
+                    "num_syllables" => result.num_syllables = value.parse().ok(),
                     "stress" => result.stress = string_or_none(value),
-                    "minfrequency" => {
-                        if let Ok(min_frequency) = value.parse() {
-                            result.min_frequency = Some(min_frequency);
-                        }
-                    }
-                    "maxfrequency" => {
-                        if let Ok(max_frequency) = value.parse() {
-                            result.max_frequency = Some(max_frequency);
-                        }
-                    }
-                    "pos" => result.part_of_speech = string_or_none(value),
+                    "morph_orthography" => result.morph_orthography = string_or_none(value),
+                    "morph_transcription" => result.morph_transcription = string_or_none(value),
+                    "num_morphemes" => result.num_morphemes = value.parse().ok(),
+                    "min_frequency" => result.min_frequency = value.parse().ok(),
+                    "max_frequency" => result.max_frequency = value.parse().ok(),
+                    "part_of_speech" => result.part_of_speech = string_or_none(value),
                     _ => (),
                 }
             }
@@ -104,35 +101,52 @@ fn parse_query(query: &str) -> Query {
     result
 }
 
+fn clean_regex(re: &str) -> Regex {
+    let re = re
+        .replace(r"\", r"\\")
+        .replace('[', r"\[")
+        .replace('{', r"\{")
+        .replace(r"(?", r"(\?");
+    Regex::new(re.as_str()).unwrap()
+}
+
 fn get_result(query: &Query) -> io::Result<Vec<Word>> {
     let dictionary = BufReader::new(File::open("elpdic")?);
     let mut result = Vec::new();
-    let orthography_regex = Regex::new(&query.orthography).unwrap();
-    let transcription_regex = Regex::new(&query.transcription).unwrap();
-    let mut stress_regex = None;
-    if let Some(stress) = &query.stress {
-        stress_regex = Some(Regex::new(stress).unwrap());
-    }
-    let mut pos_regex = None;
-    if let Some(pos) = &query.part_of_speech {
-        pos_regex = Some(Regex::new(pos).unwrap());
-    }
+    let orthography_regex = clean_regex(&query.orthography);
+    let transcription_regex = clean_regex(&query.transcription);
+    let stress_regex = match &query.stress {
+        Some(stress) => Some(clean_regex(stress)),
+        None => None,
+    };
+    let pos_regex = match &query.part_of_speech {
+        Some(pos) => Some(clean_regex(pos)),
+        None => None,
+    };
+    let morph_orthography_regex = match &query.morph_orthography {
+        Some(morph_orthography) => Some(clean_regex(morph_orthography)),
+        None => None,
+    };
+    let morph_transcription_regex = match &query.morph_transcription {
+        Some(morph_transcription) => Some(clean_regex(morph_transcription)),
+        None => None,
+    };
     for line in dictionary.lines() {
         let line = line.unwrap();
         let columns: Vec<&str> = line.split('\t').collect();
         let word = Word {
-            spelling: String::from(columns[0]),
+            orthography: String::from(columns[0]),
             transcription: String::from(columns[1]),
             num_syllables: columns[2].parse().unwrap_or(0),
             stress: String::from(columns[3]),
-            spelling_morph: String::from(columns[4]),
-            transcription_morph: String::from(columns[5]),
+            morph_orthography: String::from(columns[4]),
+            morph_transcription: String::from(columns[5]),
             num_morphemes: columns[6].parse().unwrap_or(0),
             part_of_speech: String::from(columns[7]),
             hal_frequency: columns[8].parse().unwrap_or(0),
             hal_frequency_log: columns[9].parse().unwrap_or(0f32),
         };
-        if !(orthography_regex.is_match(word.spelling.as_str())
+        if !(orthography_regex.is_match(word.orthography.as_str())
             && transcription_regex.is_match(word.transcription.as_str()))
         {
             continue;
@@ -146,6 +160,21 @@ fn get_result(query: &Query) -> io::Result<Vec<Word>> {
             if !stress_regex.is_match(word.stress.as_str()) {
                 continue;
             }
+        }
+        if let Some(morph_orthography_regex) = &morph_orthography_regex {
+            if !morph_orthography_regex.is_match(word.morph_orthography.as_str()) {
+                continue;
+            }
+        }
+        if let Some(morph_transcription_regex) = &morph_transcription_regex {
+            if !morph_transcription_regex.is_match(word.morph_transcription.as_str()) {
+                continue;
+            }
+        }
+        if let Some(num_morphemes) = query.num_morphemes {
+            if word.num_morphemes != num_morphemes {
+                continue;
+            };
         }
         if let Some(min_frequency) = query.min_frequency {
             if word.hal_frequency < min_frequency {
